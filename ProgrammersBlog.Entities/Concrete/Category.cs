97a53d0f0293ee6522eb7be5fa789d7a7954bdbb
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-07-03 07:40:13 +0300

    DataLayerRepositories
    Interfaces for DataAccess Layer are created. In addition, Duplicated class IEntityRepository is deleted. Category class in EntityL is fixed.

--- ProgrammersBlog.Entities/Concrete/Category.cs
+++ ProgrammersBlog.Entities/Concrete/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Concrete
 {
-    public class Category
+    public class Category : EntityBase, IEntity
     {
         public string Name { get; set; }
         public string Description { get; set; }
