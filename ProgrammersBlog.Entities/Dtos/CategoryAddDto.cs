18fe4d10803628b81e1a2471eae5f1a7c102ea0e
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-07-04 03:41:38 +0300

    CategoryService && CategoryUpdate
    CategoryService && CategoryUpdate were created.

---
+++ ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adi")]
+        [Required(ErrorMessage = "{0} bos gecilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden fazla olmamalidir")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalidir")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Aciklamasi")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olmamalidir")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalidir")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Ozel Not Alani")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olmamalidir")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalidir")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} bos gecilmemelidir.")]
+        public bool IsActive { get; set; }
+    }
+}
