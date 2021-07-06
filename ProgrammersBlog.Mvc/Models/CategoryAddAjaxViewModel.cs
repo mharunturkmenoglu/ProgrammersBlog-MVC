7dfc8159d5a5e21fd2fe01e2ca13026b7b337f1d
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-07-06 04:02:35 +0300

    AjaxPost
    CategoryAddAjaxViewModel.cs is created. Http Post op is done.

---
+++ ProgrammersBlog.Mvc/Models/CategoryAddAjaxViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ProgrammersBlog.Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Mvc.Models
+{
+    public class CategoryAddAjaxViewModel
+    {
+        public CategoryAddDto CategoryAddDto { get; set; }
+        public string  CategotyAddPartial { get; set; }
+        public CategoryDto CategoryDto { get; set; }
+    }
+}
