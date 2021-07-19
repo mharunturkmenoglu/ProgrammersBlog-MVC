d21cfcd710d30b808c48f7083d57deaddc28edfe
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-07-19 17:31:12 +0300

    ViewComponents
    AdminMenu and UserMenu viewcomponents are created and implemented.

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Models/UserWithRolesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using ProgrammersBlog.Entities.Concrete;
+
+namespace ProgrammersBlog.Mvc.Areas.Admin.Models
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
