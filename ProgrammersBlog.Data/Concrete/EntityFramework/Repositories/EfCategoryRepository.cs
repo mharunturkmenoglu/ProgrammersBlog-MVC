d338c1fc118aeffb1d0ff12bff58b3aed7388941
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-07-24 15:04:13 +0300

    Update EfRepositoryBase
    EfRepositoryBase refactoring.

--- ProgrammersBlog.Data/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
 
 namespace ProgrammersBlog.Data.Concrete.EntityFramework.Repositories
 {
@@ -14,6 +15,19 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Repositories
     {
         public EfCategoryRepository(DbContext context) : base(context)
         {
+        }
+
+        public async Task<Category> GetById(int categoryId)
+        {
+            return await ProgrammersBlogContext.Categories.SingleOrDefaultAsync(c => c.Id == categoryId);
+        }
+
+        private ProgrammersBlogContext ProgrammersBlogContext
+        {
+            get
+            {
+                return _context as ProgrammersBlogContext;
+            }
         }
     }
 }
--- ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ ProgrammersBlog.Shared/Data/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -12,7 +12,7 @@ namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
 {
     public class EfEntityRepositoryBase<TEntity> : IEntityRepository<TEntity> where TEntity : class, IEntity, new()
     {
-        private readonly DbContext _context;
+        protected readonly DbContext _context;
 
         public EfEntityRepositoryBase(DbContext context)
         {
@@ -43,7 +43,7 @@ namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
         public async Task<IList<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate = null, params Expression<Func<TEntity, object>>[] includeProperties)
         {
             IQueryable<TEntity> query = _context.Set<TEntity>();
-            if(predicate != null)
+            if (predicate != null)
             {
                 query = query.Where(predicate);
             }
@@ -61,10 +61,7 @@ namespace ProgrammersBlog.Shared.Data.Concrete.EntityFramework
         public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
         {
             IQueryable<TEntity> query = _context.Set<TEntity>();
-            if (predicate != null)
-            {
-                query = query.Where(predicate);
-            }
+            query = query.Where(predicate);
 
             if (includeProperties.Any())
             {
