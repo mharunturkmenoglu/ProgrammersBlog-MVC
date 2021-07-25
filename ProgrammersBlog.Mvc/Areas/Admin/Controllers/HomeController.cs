03da405a97074916b2a0c9965c69fc33c1e48293
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-07-26 01:51:09 +0300

    Update Dashboard
    Dashboard is static now.

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -4,6 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using ProgrammersBlog.Shared.Utilities.Results.Concrete;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
 {
@@ -11,9 +19,39 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService _categoryService;
+        private readonly IArticleService _articleService;
+        private readonly ICommentService _commentService;
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(UserManager<User> userManager, ICommentService commentService, IArticleService articleService, ICategoryService categoryService)
         {
-            return View();
+            _userManager = userManager;
+            _commentService = commentService;
+            _articleService = articleService;
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categoriesCountResult = await _categoryService.CountByIsDeleted();
+            var articlesCountResult = await _articleService.CountByIsDeleted();
+            var commentCountResult = await _commentService.CountByIsDeleted();
+            var userCount = await _userManager.Users.CountAsync();
+            var articlesResult = await _articleService.GetAll();
+            if (categoriesCountResult.ResultStatus == ResultStatus.Success && articlesResult.ResultStatus == ResultStatus.Success &&
+                commentCountResult.ResultStatus == ResultStatus.Success && userCount > -1 && articlesResult.ResultStatus == ResultStatus.Success) { }
+            {
+                return View(new DashboardViewModel
+                {
+                    CategoriesCount = categoriesCountResult.Data,
+                    ArticlesCount = articlesCountResult.Data,
+                    CommentsCount = commentCountResult.Data,
+                    UsersCount = userCount,
+                    Articles = articlesResult.Data
+                });
+            }
+            return NotFound();
         }
     }
 }
