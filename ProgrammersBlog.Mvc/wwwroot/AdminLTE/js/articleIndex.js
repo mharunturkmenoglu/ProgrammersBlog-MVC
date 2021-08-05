221cb927cafd1cf01959a9eb5173629ea1eeef13
Mustafa Harun Türkmenoğlu <75381086+vrMHT@users.noreply.github.com>
2021-08-05 22:55:26 +0300

    Create ArticleController
    article controller, article.js , Index page were created.

---
+++ ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleIndex.js
@@ -0,0 +1,123 @@
+﻿$(document).ready(function () {
+
+    /* DataTables start here. */
+
+    const dataTable = $('#articlesTable').DataTable({
+        dom:
+            "<'row'<'col-sm-3'l><'col-sm-6 text-center'B><'col-sm-3'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+        buttons: [
+            {
+                text: 'Ekle',
+                attr: {
+                    id: "btnAdd",
+                },
+                className: 'btn btn-success',
+                action: function (e, dt, node, config) {
+                }
+            },
+            {
+                text: 'Yenile',
+                className: 'btn btn-warning',
+                action: function (e, dt, node, config) {
+                    $.ajax({
+                        type: 'GET',
+                        url: '/Admin/User/GetAllUsers/',
+                        contentType: "application/json",
+                        beforeSend: function () {
+                            $('#usersTable').hide();
+                            $('.spinner-border').show();
+                        },
+                        success: function (data) {
+                            const userListDto = jQuery.parseJSON(data);
+                            dataTable.clear();
+                            console.log(userListDto);
+                            if (userListDto.ResultStatus === 0) {
+                                $.each(userListDto.Users.$values,
+                                    function (index, user) {
+                                        const newTableRow = dataTable.row.add([
+                                            user.Id,
+                                            user.UserName,
+                                            user.Email,
+                                            user.PhoneNumber,
+                                            `<img src="/img/${user.Picture}" alt="${user.UserName}" class="my-image-table" />`,
+                                            `
+                                <button class="btn btn-primary btn-sm btn-update" data-id="${user.Id}"><span class="fas fa-edit"></span></button>
+                                <button class="btn btn-danger btn-sm btn-delete" data-id="${user.Id}"><span class="fas fa-minus-circle"></span></button>
+                                            `
+                                        ]).node();
+                                        const jqueryTableRow = $(newTableRow);
+                                        jqueryTableRow.attr('name', `${user.Id}`);
+                                    });
+                                dataTable.draw();
+                                $('.spinner-border').hide();
+                                $('#usersTable').fadeIn(1400);
+                            } else {
+                                toastr.error(`${userListDto.Message}`, 'İşlem Başarısız!');
+                            }
+                        },
+                        error: function (err) {
+                            console.log(err);
+                            $('.spinner-border').hide();
+                            $('#usersTable').fadeIn(1000);
+                            toastr.error(`${err.responseText}`, 'Hata!');
+                        }
+                    });
+                }
+            }
+        ],
+        language: {
+            "sDecimal": ",",
+            "sEmptyTable": "Tabloda herhangi bir veri mevcut değil",
+            "sInfo": "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
+            "sInfoEmpty": "Kayıt yok",
+            "sInfoFiltered": "(_MAX_ kayıt içerisinden bulunan)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Sayfada _MENU_ kayıt göster",
+            "sLoadingRecords": "Yükleniyor...",
+            "sProcessing": "İşleniyor...",
+            "sSearch": "Ara:",
+            "sZeroRecords": "Eşleşen kayıt bulunamadı",
+            "oPaginate": {
+                "sFirst": "İlk",
+                "sLast": "Son",
+                "sNext": "Sonraki",
+                "sPrevious": "Önceki"
+            },
+            "oAria": {
+                "sSortAscending": ": artan sütun sıralamasını aktifleştir",
+                "sSortDescending": ": azalan sütun sıralamasını aktifleştir"
+            },
+            "select": {
+                "rows": {
+                    "_": "%d kayıt seçildi",
+                    "0": "",
+                    "1": "1 kayıt seçildi"
+                }
+            }
+        }
+    });
+
+    /* DataTables end here */
+
+    /* AJAX POST / Deleting a Article starts from here */
+
+    $(function () {
+        const url = '/Admin/User/Update/';
+        const placeHolderDiv = $('#modalPlaceHolder');
+        $(document).on('click',
+            '.btn-update',
+            function (event) {
+                event.preventDefault();
+                const id = $(this).attr('data-id');
+                $.get(url, { userId: id }).done(function (data) {
+                    placeHolderDiv.html(data);
+                    placeHolderDiv.find('.modal').modal('show');
+                }).fail(function (err) {
+                    toastr.error(`${err.responseText}`, 'Hata!');
+                });
+            });
+    });
+});
